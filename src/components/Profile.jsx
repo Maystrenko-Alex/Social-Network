ff556cc36085c592156300d5c1b39dd50699a45c
AlexMaystrenko <maystrenko_alex@mail.ru>
2021-12-22 01:21:02 +0300

    social network l-14

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 export const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img alt='images' src='https://images.ctfassets.net/hrltx12pl8hq/3BkYwz5RAnnN1Q2k7FYMir/5bc8172b8597e5c3d7fe02daa15dd371/Flex_lite_CMS_LIHP.jpg?fit=fill&w=840&h=350' />
       </div>
@@ -14,11 +15,11 @@ export const Profile = () => {
         <div>
           New post
         </div>
-        <div>
-          <div>
+        <div className={s.posts}>
+          <div className={s.item}>
             post1
           </div>
-          <div>
+          <div className={s.item}>
             post2
           </div>
         </div>
